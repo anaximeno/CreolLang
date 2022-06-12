e08fed17a99edb1da5f8b0dcd38559074da30383
anaximeno <anaximenobrito@gmail.com>
2022-06-12 21:17:41 -0100

    refactor: Set some global vars to be a unique_ptr feat: Add InitializeModule function

--- include/base.hpp
+++ include/base.hpp
@@ -2,15 +2,22 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/IRBuilder.h>
 #include <memory>
+#include <string>
 
 #ifndef _CREOL_BASE
 #define _CREOL_BASE
 
 namespace creol {
-    static llvm::LLVMContext TheContext;
+    static std::unique_ptr<llvm::LLVMContext> TheContext;
     static std::unique_ptr<llvm::Module> TheModule;
     static std::map<std::string, llvm::Value*> NamedValues;
-    static llvm::IRBuilder<> TheBuilder(TheContext);
+    static std::unique_ptr<llvm::IRBuilder<>> TheBuilder;
+
+    static void InitializeModule(const std::string& Name) {
+        TheContext = std::make_unique<llvm::LLVMContext>();
+        TheModule = std::make_unique<llvm::Module>(Name, *TheContext);
+        TheBuilder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+    }
 };
 
 #endif
