4e6ca75d8d68bb32cb521d0461132589fdff06e7
anaximeno <anaximenobrito@gmail.com>
2022-06-12 21:07:31 -0100

    feat: Add creol main header again

---
+++ include/creol.hpp
@@ -0,0 +1,12 @@
+#ifndef _CREOL_CREOL
+#define _CREOL_CREOL
+
+#include "base.hpp"
+#include "binexpr.hpp"
+#include "expr.hpp"
+#include "function.hpp"
+#include "literals.hpp"
+#include "misc.hpp"
+#include "vars.hpp"
+
+#endif
