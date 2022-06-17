1f128742695e9875cf7c962633bb123625cd23f4
anaximeno <anaximenobrito@gmail.com>
2022-06-17 20:01:36 -0100

    feat: Add compile script to the parser

---
+++ rules/compile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+COMPILER=g++
+OUTFILE=parser
+
+# Compile the scanner
+flex -o scanner.cc scanner.l
+
+if [[ $? -eq 0 ]]
+then
+    # Compile the parser
+    bison -dt parser.y -o parser.cc
+
+    if [[ $? -eq 0 ]]
+    then
+        $COMPILER -g -o $OUTFILE parser.cc scanner.cc
+        if [[ $? -eq 0 ]]
+        then
+            echo -e "\nParser sucessfully compiled!\n"
+        else
+            echo -e "\nError: could not compile the parser!"
+        fi
+
+        # Remove the generated parser
+        rm parser.cc parser.hh
+    else
+        echo -e "\nError: could not create the parser!"
+    fi
+
+    # Remove the generated parser
+    rm scanner.cc
+else
+    echo -e "\nError: could not create the scanner!"
+fi
