22eecc861be338268b03af0bb70a49d811907373
anaximeno <anaximenobrito@gmail.com>
2022-06-30 10:45:20 -0100

    feat: Implemented the main file to integrate with the parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
-#include <iostream>
+#include "include/creol.hh"
+#include "parser.hh"
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello, CreolLang!" << std::endl;
-    return 0;
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <cstdlib>
+
+extern FILE* yyin;
+extern int yyparse(void);
+extern creol::BlockSttmt* Program;
+
+namespace fs = std::filesystem;
+
+int main(int argc, const char* const* argv) {
+    if (argc != 2) {
+        std::cout << "Help Section: Execute using:" << std::endl;
+        std::cout << "$ creol <filename.crl>" << std::endl;
+        return 1;
+    }
+
+    const char* filename = argv[1];
+
+    if (!fs::exists(filename)) {
+        std::cout << "File '" << filename
+                  << "' was not found in the current dir!"
+                  << std::endl;
+        return 1;
+    }
+
+    FILE* file = fopen(filename, "r");
+
+    if (!file) {
+        std::cout << "Error opening the file '"
+                  << filename << "'!" << std::endl;
+        return 1;
+    }
+
+    yyin = file; yyparse();
+
+    std::cout << Program->CodeGen() << std::endl;
 }
