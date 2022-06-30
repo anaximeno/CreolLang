53b2501342d885a1b536158fc88a32a9e3583ca5
anaximeno <anaximenobrito@gmail.com>
2022-06-30 03:15:56 -0100

    refactor: Update

--- include/creol.hh
+++ include/creol.hh
@@ -10,25 +10,25 @@ namespace creol {
 /// Represents a statement
 class Sttmt {
 public:
-    virtual std::string codegen() = 0;
+    virtual std::string CodeGen() = 0;
 };
 
 /// Represents an expression
 class Expr : public Sttmt {
 public:
-    virtual std::string codegen() = 0;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents the declaration of a variable
 class VarDeclSttmt : public Sttmt {
 protected:
-    const std::string Type;
-    const std::string Name;
-    const Expr* Value;
+    std::string Type;
+    std::string Name;
+    Expr* Value;
 public:
     VarDeclSttmt(std::string Type, std::string Name, Expr* Value)
     : Type(Type), Name(Name), Value(Value) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents a blockSttmt of code
@@ -37,95 +37,106 @@ protected:
     std::vector<Sttmt*> SttmtList;
 public:
     void AddSttmt(Sttmt* sttmt);
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents the declaration of a function
 class FuncDeclSttmt : public Sttmt {
 protected:
-    const std::string Type;
-    const std::string Name;
+    std::string Type;
+    std::string Name;
     std::vector<VarDeclSttmt*> Args;
 public:
     FuncDeclSttmt(std::string Type, std::string Name, std::vector<VarDeclSttmt*>)
     : Type(Type), Name(Name), Args(Args) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents an if statement
 class IfSttmt : public Sttmt {
 protected:
-    const Expr* Cond;
-    const BlockSttmt* Then;
-    const BlockSttmt* Else;
+    Expr* Cond;
+    BlockSttmt* Then;
+    BlockSttmt* Else;
 public:
     IfSttmt(Expr* Cond, BlockSttmt* Then, BlockSttmt* Else)
     : Cond(Cond), Then(Then), Else(Else) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents a while loop
 class WhileSttmt : public Sttmt {
 protected:
-    const Expr* Cond;
-    const BlockSttmt* Do;
+    Expr* Cond;
+    BlockSttmt* Do;
 public:
     WhileSttmt(Expr* Cond, BlockSttmt* Do)
     : Cond(Cond), Do(Do) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents a jump statement
 class JumpSttmt : public Sttmt {
 protected:
-    const std::string Name;
+    std::string Name;
 public:
     JumpSttmt(std::string Name)
     : Name(Name) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents a return statement
 class ReturnSttmt : public JumpSttmt {
 protected:
-    const Expr* ReturnValue;
+    Expr* ReturnValue;
 public:
     ReturnSttmt(Expr* ReturnValue)
     : JumpSttmt("return"), ReturnValue(ReturnValue) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
 /// Represents the function call
 class FunCallExpr : public Expr {
 protected:
-    const std::string Name;
-    std::vector<Expr*> Args;
+    std::string Name;
+    std::vector<Expr*> Params;
 public:
-    FunCallExpr(std::string Name, std::vector<Expr*> Args)
-    : Name(Name), Args(Args) {}
-    virtual std::string codegen() override;
+    FunCallExpr(std::string Name, std::vector<Expr*> Params)
+    : Name(Name), Params(Params) {}
+    virtual std::string CodeGen() override;
 };
 
 /// Represents a binary expression
 class BinExpr : public Expr {
 protected:
-    const std::string Op;
-    const std::string LHS, RHS;
+    std::string Op;
+    Expr* LHS;
+    Expr* RHS;
 public:
-    BinExpr(std::string Op, std::string LHS, std::string RHS)
+    BinExpr(std::string Op, Expr* LHS, Expr* RHS)
     : Op(Op), LHS(LHS), RHS(RHS) {  }
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
 };
 
-/// Represents a literal (constant) expression
+/// Represents a literal (nt) expression
 class LiteralExpr : public Expr {
 protected:
-    const std::string Type;
-    const std::string Value;
+    std::string Type;
+    std::string Value;
 public:
     LiteralExpr(std::string Type, std::string Value)
     : Type(Type), Value(Value) {}
-    virtual std::string codegen() override;
+    virtual std::string CodeGen() override;
+};
+
+/// An expression statement is an expression ending by a semicolon
+class ExprSttmt : public Expr {
+protected:
+    Expr* Expression;
+public:
+    ExprSttmt(Expr* Expression)
+    : Expression(Expression) {}
+    virtual std::string CodeGen() override;
 };
 }
 
