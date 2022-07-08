86ce4065af8af42bef51576686bd04f389b4047d
anaximeno <anaximenobrito@gmail.com>
2022-07-08 16:46:43 -0100

    feat: Initital implementation of the compiler logic

--- include/creol/ast.hh
+++ include/creol/ast.hh
@@ -1,25 +1,56 @@
-#ifndef _CREOL_AST_TRANSPILER_HEADER
-#define _CREOL_AST_TRANSPILER_HEADER
+#ifndef _CREOL_AST_COMPILER_HEADER
+#define _CREOL_AST_COMPILER_HEADER
 
+// Standard libs
 #include <string>
 #include <vector>
 #include <memory>
 #include <algorithm>
 
+// LLVM libs
+#include <llvm/IR/Value.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/ADT/APFloat.h>
+#include <llvm/ADT/APSInt.h>
+#include <llvm/IR/Constants.h>
+
 namespace creol {
     namespace ast {
+        /// Represents a local context, which may be used
+        /// inside a function, statement or module.
+        class LocalContext {
+        public:
+            std::unique_ptr<llvm::Module> TheModule;
+            std::unique_ptr<llvm::LLVMContext> TheContext;
+            std::map<std::string, llvm::Value*> NamedValues;
+            std::unique_ptr<llvm::IRBuilder<>> TheBuilder;
+            /// Returns a new instance of the LocalContext class with name
+            /// Module Name.
+            static std::shared_ptr<LocalContext>New(std::string ModuleName);
+        private:
+            void Initialize(std::string ModuleName);
+        };
+
         /// Represents a statement
         class Sttmt {
         public:
-            virtual std::string CodeGen() = 0;
+            virtual llvm::Value* CodeGen(LocalContext LC) = 0;
         };
 
         /// Represents an expression
         class Expr : public Sttmt {
         public:
-            virtual std::string CodeGen() override;
+            virtual llvm::Value* CodeGen(LocalContext LC) override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents the declaration of a variable
         class VarDeclSttmt : public Sttmt {
         protected:
@@ -33,6 +64,7 @@ namespace creol {
             void SetType(std::string Type);
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a blockSttmt of code
         class BlockSttmt : public Sttmt {
         protected:
@@ -45,6 +77,8 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
+        /// Represents the arguments of the function
         class FuncArgs {
         protected:
             std::vector<VarDeclSttmt*> Args;
@@ -53,6 +87,7 @@ namespace creol {
             virtual std::string CodeGen();
         };
 
+        /// TODO: Implement in LLVM
         /// Represents the declaration of a function
         class FuncDeclSttmt : public Sttmt {
         protected:
@@ -67,6 +102,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents an if statement
         class IfSttmt : public Sttmt {
         protected:
@@ -79,6 +115,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a while loop
         class WhileSttmt : public Sttmt {
         protected:
@@ -90,6 +127,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a jump statement
         class JumpSttmt : public Sttmt {
         protected:
@@ -100,6 +138,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a return statement
         class ReturnSttmt : public JumpSttmt {
         protected:
@@ -110,6 +149,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         class FuncCallArgs {
         protected:
             std::vector<Expr*> Args;
@@ -118,6 +158,7 @@ namespace creol {
             virtual std::string CodeGen();
         };
 
+        /// TODO: Implement in LLVM
         /// Represents the function call
         class FunCallExpr : public Expr {
         protected:
@@ -129,6 +170,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a binary expression
         class BinExpr : public Expr {
         protected:
@@ -141,6 +183,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a literal (nt) expression
         class LiteralExpr : public Expr {
         protected:
@@ -155,6 +198,7 @@ namespace creol {
             void ActivateAutoCast(void);
         };
 
+        /// TODO: Implement in LLVM
         /// An expression statement is an expression ending by a semicolon
         class ExprSttmt : public Expr {
         protected:
@@ -165,6 +209,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a identifier
         class IdentExpr : public Expr {
         protected:
@@ -175,6 +220,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents an expression sorrounded by parentheses
         class ParExpr : public Expr {
         protected:
@@ -185,6 +231,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents an assignment to a variable
         class AssignExpr : public Expr {
         protected:
@@ -197,6 +244,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Represents the for statement
         class ForSttmt : public Sttmt {
         protected:
@@ -210,6 +258,7 @@ namespace creol {
             virtual std::string CodeGen() override;
         };
 
+        /// TODO: Implement in LLVM
         /// Maps the 'mostra' keyword to printf in C
         class MostraFunCallExpr : public FunCallExpr {
         public:
@@ -217,6 +266,7 @@ namespace creol {
             : FunCallExpr("printf", Args) {}
         };
 
+        /// TODO: Implement in LLVM
         /// Represents a library importation
         class ImportSttmt : public Sttmt {
         protected:
@@ -229,4 +279,4 @@ namespace creol {
     };
 };
 
-#endif // _CREOL_AST_TRANSPILER_HEADER
+#endif // _CREOL_AST_COMPILER_HEADER
--- src/creol/ast.cc
+++ src/creol/ast.cc
@@ -2,24 +2,40 @@
 
 using namespace creol;
 
+void ast::LocalContext::Initialize(std::string ModuleName) {
+    TheContext = std::make_unique<llvm::LLVMContext>();
+    TheModule = std::make_unique<llvm::Module>(ModuleName, *TheContext);
+    TheBuilder = std::make_unique<llvm::IRBuilder<>>(*TheContext);
+}
+
+static std::shared_ptr<ast::LocalContext> ast::LocalContext::New(std::string ModuleName) {
+    std::shared_ptr<ast::LocalContext> LC = std::make_shared<ast::LocalContext>();
+    LC->Initialize();
+    return LC;
+}
+
+/// TODO: Implement in LLVM
 std::string ast::Expr::CodeGen() {
     return "";
 }
 
+/// TODO: Implement in LLVM
 std::string ast::VarDeclSttmt::CodeGen() {
     std::string VarVal = Value ? " = " + Value->CodeGen() : "";
     return Type + ' ' + Name + VarVal;
 }
 
+/// TODO: Implement in LLVM
 void ast::VarDeclSttmt::SetType(std::string Type) {
     this->Type = Type;
 }
 
-
+/// TODO: Implement in LLVM
 void ast::BlockSttmt::AddSttmt(ast::Sttmt* sttmt) {
     SttmtList.push_back(sttmt);
 }
 
+/// TODO: Implement in LLVM
 std::string ast::BlockSttmt::CodeGen() {
     std::string block = "";
 
@@ -33,18 +49,22 @@ std::string ast::BlockSttmt::CodeGen() {
     return block;
 }
 
+/// TODO: Implement in LLVM
 void ast::BlockSttmt::UseBrackets() {
     BracketsOn = true;
 }
 
+/// TODO: Implement in LLVM
 void ast::BlockSttmt::DontUseBrackets() {
     BracketsOn = false;
 }
 
+/// TODO: Implement in LLVM
 void ast::FuncArgs::AddArg(ast::VarDeclSttmt* Arg) {
     Args.push_back(Arg);
 }
 
+/// TODO: Implement in LLVM
 std::string ast::FuncArgs::CodeGen() {
     std::string Arguments = "";
 
@@ -58,32 +78,39 @@ std::string ast::FuncArgs::CodeGen() {
     return Arguments;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::FuncDeclSttmt::CodeGen() {
     return Type + " " + Name + " ( " + Args->CodeGen() + " ) " + Body->CodeGen();
 }
 
+/// TODO: Implement in LLVM
 std::string ast::IfSttmt::CodeGen() {
     auto E = Else ? " else " + Else->CodeGen() : "";
     return "if ( " + Cond->CodeGen() + " ) " + Then->CodeGen() + E;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::WhileSttmt::CodeGen() {
     return "while ( " + Cond->CodeGen() + " )" + Do->CodeGen();
 }
 
+/// TODO: Implement in LLVM
 std::string ast::JumpSttmt::CodeGen() {
     return Name + ";";
 }
 
+/// TODO: Implement in LLVM
 std::string ast::ReturnSttmt::CodeGen() {
     auto R = ReturnValue ? ReturnValue->CodeGen() : "";
     return "return " + R + ";";
 }
 
+/// TODO: Implement in LLVM
 void ast::FuncCallArgs::AddArg(Expr* Arg) {
     Args.push_back(Arg);
 }
 
+/// TODO: Implement in LLVM
 std::string ast::FuncCallArgs::CodeGen() {
     std::string Arguments = "";
 
@@ -97,47 +124,57 @@ std::string ast::FuncCallArgs::CodeGen() {
     return Arguments;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::FunCallExpr::CodeGen() {
     auto A = Args ? Args->CodeGen() : "";
     return Name + "( " + A + " )";
 }
 
+/// TODO: Implement in LLVM
 std::string ast::BinExpr::CodeGen() {
     auto L = LHS ? LHS->CodeGen() : "";
     auto R = RHS ? RHS->CodeGen() : "";
     return L + " " + Op + " " + R;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::LiteralExpr::CodeGen() {
     auto Prefix = AutoCast ? "(" + Type + ") " : "";
     return Prefix + Value;
 }
 
+/// TODO: Implement in LLVM
 void ast::LiteralExpr::ActivateAutoCast() {
     AutoCast = true;
 }
 
+/// TODO: Implement in LLVM
 void ast::LiteralExpr::DeactivateAutoCast() {
     AutoCast = false;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::ExprSttmt::CodeGen() {
     return Expression ? Expression->CodeGen() + ";" : ";";
 }
 
+/// TODO: Implement in LLVM
 std::string ast::IdentExpr::CodeGen() {
     return Name;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::ParExpr::CodeGen() {
     auto C = Content ? Content->CodeGen() : "";
     return "( " + C + " )";
 }
 
+/// TODO: Implement in LLVM
 std::string ast::AssignExpr::CodeGen() {
     return Assignee->CodeGen() + " = " + Assigned->CodeGen();
 }
 
+/// TODO: Implement in LLVM
 std::string ast::ForSttmt::CodeGen() {
     auto S = Start ? Start->CodeGen() : "";
     auto C = Cond ? Cond->CodeGen() : "";
@@ -146,6 +183,7 @@ std::string ast::ForSttmt::CodeGen() {
     return "for (" + S + " ; " + C + " ; " + A + " ) " + T;
 }
 
+/// TODO: Implement in LLVM
 std::string ast::ImportSttmt::CodeGen() {
     return "#include " + Import + "\n";
 }
